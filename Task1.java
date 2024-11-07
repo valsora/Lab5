72a7ee8f0b13c9bdb2f63e213628362aa7c36122
valsora <valsora9@gmail.com>
2024-11-07 17:11:27 +0300

    1

--- Task1.java
+++ Task1.java
@@ -3,8 +3,8 @@ import java.util.regex.Pattern;
 
 public class Task1 {
     public static void main(String[] args) {
-        String text = "The price of the product №3 is $19.99";
-        Pattern pattern = Pattern.compile("\\d+(\\.\\d+)?");
+        String text = "The price of the product №3 is 19.99$. The temperature outside is -1°C. 6 - 2 = 4.";
+        Pattern pattern = Pattern.compile("(-)?\\d+(\\.\\d+)?");
         Matcher matcher = pattern.matcher(text);
         while (matcher.find()) System.out.println(matcher.group());
     }
